43111366b4855ff21f99eb319324992d26c8830a
Arvid Tomayko-Peters <arvidtp@gmail.com>
2014-06-08 16:27:24 -0400

    loop report for "playback limits" mode when playing forward or backward

--- jit.gl.hap.c
+++ jit.gl.hap.c
@@ -833,6 +833,17 @@ void jit_gl_hap_do_report(t_jit_gl_hap *x)
 					jit_gl_hap_looper_notify(x);
 				}
 			}
+			else if(x->loop == JIT_GL_HAP_LOOP_LIMITS) {
+				if (x->rate>0) {
+					if ((curtime >= x->looppoints[1]) && (curtime>x->prevtime)) {
+						jit_gl_hap_looper_notify(x);
+					}
+				} else {
+					if ((curtime <= x->looppoints[0]) && (curtime<x->prevtime)) {
+						jit_gl_hap_looper_notify(x);
+					}
+				}
+			}
 		}
 		
 		if(x->loop == JIT_GL_HAP_LOOP_PALINDROME)
